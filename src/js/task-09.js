9f61aac12367e35addca409adc5c31389c6959bf
Denis Serkhel <ripvanfish27@gmail.com>
2021-12-18 19:49:12 +0200

    9я complete

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
 }
 
 
+const btnRef = document.querySelector(".change-color");
+const bodyRef = document.querySelector("body");
+const spanRef = document.querySelector(".color");
+console.log(bodyRef);
+
+btnRef.addEventListener("click", makeRandomBodyColor)
+
+function makeRandomBodyColor(event) {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+  spanRef.textContent = getRandomHexColor();
+}
