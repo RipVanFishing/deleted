a989663968369c1e61424e63b8d30ba25603420a
RipVanFishing <88795676+RipVanFishing@users.noreply.github.com>
2021-12-18 20:17:51 +0200

    Delete src directory

--- src/js/task-05.js
+++
@@ -1,7 +0,0 @@
-const inputRef = document.querySelector("#name-input");
-console.log(inputRef);
-const outputRef = document.querySelector("#name-output");
-
-inputRef.addEventListener("input", (event) => {
- outputRef.textContent = event.target.value;
-})
--- src/js/task-07.js
+++
@@ -1,8 +0,0 @@
-const sliderRef = document.querySelector("#font-size-control");
-const spanRef = document.querySelector("#text");
-
-sliderRef.addEventListener("input", () => {
-    spanRef.style.fontSize = sliderRef.value + "px";
-})
-
-
