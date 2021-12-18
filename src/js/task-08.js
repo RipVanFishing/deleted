1271ab54299782738333f37ff36850b966a4dd7f
Denis Serkhel <ripvanfish27@gmail.com>
2021-12-18 14:50:34 +0200

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,24 @@
+const formRef = document.querySelector(".login-form");
+console.log(formRef);
 
+formRef.addEventListener("submit", onSubmitForm)
+
+
+function onSubmitForm(event) {
+ event.preventDefault();
+   const {
+    elements: { email, password }
+    } = event.currentTarget;
+ if (email.value === "" || password.value === "") {
+    return alert("Все поля должны быть заполнены!");
+    }  
+    const newData = {
+        email: value,
+        password:value
+    }
+    console.log(newData);
+ console.log(`Email: ${email.value}, Password: ${password.value}`);
+    event.currentTarget.reset();
+
+    
+}
